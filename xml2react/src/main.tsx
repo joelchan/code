ad16ce334eceb1f92e725e16fcdb471d69057864
Matt Erhart <mattjerhart@gmail.com>
2018-04-13 10:12:26 -0700

    added figures

--- xml2react/src/main.tsx
+++ xml2react/src/main.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as cheerio from 'cheerio';
 import * as glamorous from 'glamorous';
-import { ThemeProvider, Div } from 'glamorous';
+import { ThemeProvider, Div, Img } from 'glamorous';
 import * as reactStringReplace from 'react-string-replace';
-import * as d3 from 'd3'
-var colorScale  = d3.scaleOrdinal(d3.schemePastel1) as any;
-console.log(colorScale(0).toString())
+import * as d3 from 'd3';
+var colorScale = d3.scaleOrdinal(d3.schemePastel1) as any;
+console.log(colorScale(0).toString());
 const theme = {
   main: { color: 'red' }
 };
@@ -16,6 +16,16 @@ var $ = cheerio.load(xml, {
   xmlMode: true
 });
 
+
+let imgsCaptions: {[ids:string]: string}[] = $('fig').toArray()
+.map((el, i) => {
+  const href = $(el).children('graphic').attr("xlink:href");
+  const caption = $(el).children('caption').text().trim();
+  return {href, caption}
+});
+
+console.log(imgsCaptions[0])
+
 // todo: util
 let pText: string[] = $('p')
   .toArray()
@@ -23,9 +33,6 @@ let pText: string[] = $('p')
     return $(el).text();
   });
 
-const ReadingColumn = () => {};
-
-const LinkingColumn = () => {};
 
 class App extends React.Component<{ text: string[] }, any> {
   state = {
@@ -34,10 +41,10 @@ class App extends React.Component<{ text: string[] }, any> {
 
   updateStateFromSelectedText = () => {
     const text = window
-        .getSelection()
-        .toString()
-        .trim()
-    if (text.length > 0)     {
+      .getSelection()
+      .toString()
+      .trim();
+    if (text.length > 0) {
       this.setState({
         highlightedPhrase: text
       });
@@ -54,7 +61,7 @@ class App extends React.Component<{ text: string[] }, any> {
     let divs = this.props.text.map((t, ti) => {
       return (
         <Div key={ti} onMouseUp={(e) => this.updateStateFromSelectedText()}>
-        <Div background={colorScale(ti%9)}>{ti + 1}</Div>            
+          <Div background={colorScale(ti % 9)}>{ti + 1}</Div>
           <Highlight text={t} toMatch={wordPattern} />
         </Div>
       );
@@ -64,11 +71,13 @@ class App extends React.Component<{ text: string[] }, any> {
       if (retext !== null) {
         return (
           <Div key={ti} onMouseUp={(e) => this.updateStateFromSelectedText()}>
-            <Div background={colorScale(ti%9)}>{ti+1}</Div>            
-            {retext.map((retext, i)=>{
-              return  <Div key={i}>
-                <Highlight  text={retext + '.'} toMatch={wordPattern} />
-              </Div>
+            <Div background={colorScale(ti % 9)}>{ti + 1}</Div>
+            {retext.map((retext, i) => {
+              return (
+                <Div key={i}>
+                  <Highlight text={retext + '.'} toMatch={wordPattern} />
+                </Div>
+              );
             })}
           </Div>
         );
@@ -76,13 +85,34 @@ class App extends React.Component<{ text: string[] }, any> {
         return null;
       }
     });
+
     return (
-      <Div display={'flex'} flexDirection="row">
-        <Div flex={1} paddingRight='10px'>{divs}</Div>
-        <Div flex={1} paddingLeft='10px' borderLeft='1px solid black'>{divsWithMatch}</Div>
+      <Div>
+        <Div display='flex' onMouseUp={(e) => this.updateStateFromSelectedText()}>{imgsWithMatch(wordPattern)}</Div>
+        <hr/>
+        <Div display={'flex'} flexDirection="row">
+          <Div flex={1} paddingRight="10px">
+            {divs}
+          </Div>
+          <Div flex={1} paddingLeft="10px" borderLeft="1px solid black">
+            {divsWithMatch}
+          </Div>
+        </Div>
       </Div>
     );
   }
+}
+
+function imgsWithMatch(wordPattern) {
+ return imgsCaptions.map((imgCap, i) => {
+    return (
+      <Div>
+        <Img maxWidth={'45vw'} src={require("@assets/" + imgCap.href)} />
+        <Div>
+        <Highlight text={imgCap.caption} toMatch={wordPattern} /> </Div>
+      </Div>
+    )
+})
 }
 
 function Highlight(props: { text: string; toMatch: string | RegExp }) {
