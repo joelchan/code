658d26512319e26d388132caedb60914a35cb8e9
Matt Erhart <mattjerhart@gmail.com>
2018-05-29 17:47:50 -0700

    filter to 17k papers. clean up code.

--- pyNLP/textProcessing/readS2Corpus.py
+++ pyNLP/textProcessing/readS2Corpus.py
@@ -1,33 +1,23 @@
-import gzip, os, sys, csv
-from pathlib import Path
-import json
-import dask.bag as bag
-from dask.diagnostics import ProgressBar
-from dask.distributed import Client
-# from ipyparallel import Client
+import csv, json
+import os.path.join as join
+import dask.bag as bag # a bag is an unsorted list that we can work with in parallel
+from dask.distributed import Client #e.g. Client(processes=False, threads_per_worker=4, n_workers=1, memory_limit='3GB')
+from operator import itemgetter #for faster sorting
 
-ProgressBar().register()
-import time
-from operator import itemgetter
+ioRoot = 'E:\\code\\pyNLP\\textProcessing\\corpus-2018-05-03'
+ioPaths = {
+    'corpusAll':    join(ioRoot, "txt", "s2-corpus-*.txt"),
+    'corpusFirst':  join(ioRoot, "txt", "s2-corpus-00.txt"),
+    'sample':       join(ioRoot, "sample-S2-records.gz"),
+    'filtered':     join(ioRoot, "filtered")
+}
 
-global __file__ #relative paths that work in ipython dev or some other relative docker path
-__file__ = globals().get('x', 'E:\\code\\pyNLP\\textProcessing')
-thisFilesPath = Path(os.path.dirname(os.path.realpath(__file__)))
-fileDir = os.path.dirname(os.path.realpath(__file__))
-corpusPath = 'E:\\code\\pyNLP\\textProcessing\\corpus-2018-05-03\\txt\\s2-corpus-00.txt'
-corpusDirPath = 'E:\\code\\pyNLP\\textProcessing\\corpus-2018-05-03\\txt'
-samplePath = Path('E:\\code\\pyNLP\\textProcessing\\sample-S2-records.gz')
-outDir = 'E:\\code\\pyNLP\\textProcessing\\corpus-2018-05-03\\filtered'
-# with gzip.open(corpusPath, 'r') as f:
-#     corpusString = f.read().decode('utf-8')
-# print(time.process_time())
-# # papers = [json.loads(p) for p in corpusString.strip().split('\n')]
 def corpusFilesToDaskBag(pathOrGlobWithFiles):
     return bag.read_text(str( pathOrGlobWithFiles ), encoding='utf-8', linedelimiter='\n')\
                 .str.strip().map(json.loads)
 
-def saveCsv(listOfTuples):
-    with open(corpusDirPath + '/journal_counts.csv', 'w', newline='',encoding='utf-8') as o:
+def saveCsv(listOfTuples, outFileName):
+    with open(ioPaths.save + '\\' + outFileName, 'w', newline='',encoding='utf-8') as o:
         csv_out = csv.writer(o)
         csv_out.writerow(['name', 'freq'])
         for row in listOfTuples:
@@ -36,31 +26,17 @@ def saveCsv(listOfTuples):
 
 # if __name__ == '__main__':
 client = Client(processes=False, threads_per_worker=4, n_workers=1, memory_limit='3GB')
-#%%time
 
-from corpusFilters import filterByJournal
+def calcFrequences(bag, propName):
+    freqs = bag.pluck(propName).frequencies()
+    return sorted(freqs.compute(), key=itemgetter(1), reverse=True)
 
-bag1 = corpusFilesToDaskBag(corpusPath);
-bag2 = bag1.filter(filterByJournal)
-bag2.to_textfiles(outDir, name_function=None, compression='infer',
-             encoding='utf-8', compute=True, get=None, storage_options=None)
-# print(len(filteredArticles))
+from corpusFilters import filterByVenue
 
-
-
-#%%
-
-# print(set([p['journalName'] for p in papers if 'IEEE' in p['journalName']]))
-#%%
+bag1 = corpusFilesToDaskBag(ioPaths.corpusAll)
+bag2 = bag1.filter(filterByVenue)
+# bag of dicts needs .map(json.dumps). bag of strings doesnt
+bag2.map(json.dumps).to_textfiles(join(ioPaths.filtered, 'venueFiltered-*.json'))
 
 
-# papers = []
-# for i,s in enumerate(corpusString.strip().split('\n')):
-#     try: # deal with entries that error
-#         papers.append(json.loads(s))
-#     except:
-#         print('error for: ', i)
-#         continue
-# # print(len(papers))
 
-#%%
