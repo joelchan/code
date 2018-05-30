beb0e5d71f08e0d626619f9fcab16d2c31f9864a
Matt Erhart <mattjerhart@gmail.com>
2018-05-30 14:45:08 -0700

    tsne with nearest neighbors

--- pyNLP/gensimFastText.py
+++ pyNLP/gensimFastText.py
@@ -30,7 +30,7 @@ client.close()
 #%% Base model
 startTime = time.clock()
 model = None
-model = FastText(titles[:100000], min_count=1, workers=4)
+model = FastText(titles[:100000], min_count=1, workers=4, sg=0)
 print(time.clock() - startTime)
 model.most_similar(positive=['cognitive'])
 
@@ -41,8 +41,33 @@ model2 = FastText(titles[:100000], min_count=1, workers=4, sg=1)
 print(time.clock() - startTime)
 r1 = model.most_similar(positive=['cognitive'])
 r2 = model2.most_similar(positive=['cognitive'])
-print(tabulate([[r1[i][0], r2[i][0]] for i, x in enumerate(r1)]))
-# sg=1
+print(tabulate([[r2[i][0], r1[i][0]] for i, x in enumerate(r1)],
+               headers=['Model2', 'Model1'],
+               tablefmt='orgtbl'))
+
+#%% TSNE
+words = ['cognitive', 'sensemaking', 'comprehension', 'reading',
+         'articles', 'perception', 'notetaking', 'annotation', 'foraging',
+         'formalization', 'ontology', 'NLP', 'machine learning', 'neural',
+         'brain', 'intelligence', 'HCI', 'interaction', 'design']
+
+neighbors = [model2.most_similar(positive=[w],topn=1)[0][0] for w in words]
+
+vecs = [model2.wv[w] for w in words]
+
+import numpy as np
+from sklearn.manifold import TSNE
+X_embedded = TSNE(n_components=2, perplexity=4).fit_transform(vecs)
+X_embedded.shape
+import matplotlib.pyplot as plt
+(fig, ax) = plt.subplots(figsize=(15, 8))
+ax.scatter(X_embedded[:,0], X_embedded[:,1], c="b")
+for i, txt in enumerate(words):
+    ax.annotate(txt + "\n" + neighbors[i], (X_embedded[i,0]+.75, X_embedded[i,1]))
+# ax.scatter(X[green, 0], X[green, 1], c="g")
+# sg=1, predict the word from the context, might need less data than CBOW/sg=0
+
 # todo: gensim.models.phrases
+# todo: remove punc
 
 #%%
