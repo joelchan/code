ecb51e7e9db089f669f99e2f170fa56522e17560
Matt Erhart <mattjerhart@gmail.com>
2018-07-18 12:08:06 -0700

    can serve processed pdfs via express server

--- pdfToXml/server/index.mjs
+++ pdfToXml/server/index.mjs
@@ -1,25 +1,24 @@
+const express = require('express')
+const app = express()
 
-var puppeteer = require('puppeteer')
+// ;
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+const dirRoot = 'E:/code/pdfToXml/processed_pdfs/'
+const dirList = ['ExpertFacilitation', 'KnowledgeSharing', 'Sensemaking']
+
+dirList.forEach(dirName => {
+    app.use('/' + dirName, express.static(dirRoot + dirName, {
+        index: 'index.html'
+    }))
+    app.get('/' + dirName, (req, res) => {
+        res.sendFile('index.html', {root: staticDir})
+    })
+})
 
 
-
-async function ssr(url) {
-  //ssr = server side rendering
-  // this converts a jquery-ified page into a single html file.
-  const browser = await puppeteer.launch({headless: true});
-  const page = await browser.newPage();
-  await page.goto(url, {waitUntil: 'networkidle0'});
-  const html = await page.content(); // serialized HTML of page DOM.
-  await browser.close();
-  return html;
-}
-
-var fs = require('fs');
-
-
-ssr('http://localhost:3000').then(res => {
-  fs.writeFile("clean.html", res, function(err){
-    if (err) throw err;
-     console.log("success");
-    }); 
+app.listen(5656, () => {
+    console.log('http://localhost:5656')
 })
